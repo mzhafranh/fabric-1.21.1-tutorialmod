9a3e3b31c90f6d75a61a5935ddf2df6ea66e8dc8
mzhafranh <88304603+mzhafranh@users.noreply.github.com>
2025-06-08 19:54:01 +0700

    Init from template

---
+++ src/main/java/com/zxmasq/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package com.zxmasq.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
