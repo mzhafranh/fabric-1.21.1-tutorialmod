a083c17a9a1f9d01a01a743552eeb0debb04442d
mzhafranh <88304603+mzhafranh@users.noreply.github.com>
2025-06-11 18:53:19 +0700

    Added custom item groups

--- src/main/java/com/zxmasq/tutorialmod/TutorialMod.java
+++ src/main/java/com/zxmasq/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package com.zxmasq.tutorialmod;
 
 import com.zxmasq.tutorialmod.block.ModBlocks;
+import com.zxmasq.tutorialmod.item.ModItemGroups;
 import com.zxmasq.tutorialmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
@@ -13,6 +14,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 	}
---
+++ src/main/java/com/zxmasq/tutorialmod/item/ModItemGroups.java
@@ -0,0 +1,37 @@
+package com.zxmasq.tutorialmod.item;
+
+import com.zxmasq.tutorialmod.TutorialMod;
+import com.zxmasq.tutorialmod.block.ModBlocks;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ModItemGroups {
+    public static final ItemGroup COMMONIUM_ITEMS_GROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(TutorialMod.MOD_ID, "commonium_items"),
+            FabricItemGroup.builder()
+                    .icon(() -> new ItemStack(ModItems.COMMONIUM))
+                    .displayName(Text.translatable("itemgroup.tutorialmod.commonium_items"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(ModItems.COMMONIUM);
+                        entries.add(ModItems.RAW_COMMONIUM);
+                    }).build());
+
+    public static final ItemGroup COMMONIUM_BLOCKS_GROUP = Registry.register(Registries.ITEM_GROUP,
+            Identifier.of(TutorialMod.MOD_ID, "commonium_blocks"),
+            FabricItemGroup.builder()
+                    .icon(() -> new ItemStack(ModBlocks.COMMONIUM_BLOCK))
+                    .displayName(Text.translatable("itemgroup.tutorialmod.commonium_blocks"))
+                    .entries((displayContext, entries) -> {
+                        entries.add(ModBlocks.COMMONIUM_BLOCK);
+                        entries.add(ModBlocks.COMMONIUM_ORE);
+                    }).build());
+
+    public static void registerItemGroups(){
+        TutorialMod.LOGGER.info("Registering Item Groups for " + TutorialMod.MOD_ID);
+    }
+}
